6b82fa41b9af9f05dd7dadd87107484e8edfb40d
噢 <a23878759@163.com>
2019-09-25 16:40:45 +0800

    Initial Commit

---
+++ BiddingPriceMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BiddingPriceMall
+//
+//  Created by mac on 2019/9/25.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BiddingPriceMall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BiddingPriceMall
+//
+//  Created by mac on 2019/9/25.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
